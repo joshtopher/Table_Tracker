94e9b3298d495184bb012057b5b09acd005add0d
Josh Friedland <jefriedl@buffalo.edu>
2023-01-04 16:35:22 -0500

    Added main.py and templates

---
+++ app.py
@@ -0,0 +1,111 @@
+from flask import Flask, request, render_template, redirect
+import firebase_admin
+from flask_socketio import SocketIO, join_room, leave_room, emit
+from firebase_admin import credentials, firestore
+import random
+
+cred = credentials.Certificate("config.json")
+fb = firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+
+app = Flask(__name__)
+socket = SocketIO(app)
+
+# dict of lobby id to list of users in lobby
+lobbies = {}
+
+
+@app.get('/')
+def landing_page():
+    return render_template('landing.html')
+
+
+@app.get('/home')
+def home_page():
+    username = request.cookies.get("username")
+    if username:
+        return render_template('home.html', username=username)
+    else:
+        return redirect("/")
+
+
+@app.post('/signup')
+def register():
+    username = escape_html(request.form['username'])
+    pwd = request.form['password']
+
+    users_ref = db.collection('users').where('username', '==', username)
+    user = users_ref.get()
+    if user:
+        return "Username Taken"
+    else:
+        user_ref = db.collection('users').add({
+            'username': username,
+            'password': pwd
+        })
+        return redirect('/')
+
+
+@app.post('/login')
+def login():
+    username = escape_html(request.form['username'])
+    pwd = request.form['password']
+
+    users_ref = db.collection('users').where('username', '==', username)
+    user = users_ref.get()
+    if not user:
+        return "Username or Password is Incorrect"
+    else:
+        if user[0].to_dict()['password'] != pwd:
+            return redirect('/')
+        else:
+            response = redirect("/home")
+            response.set_cookie('username', username)
+            return response
+
+
+@app.get('/lobby/<lobby_id>')
+def join_lobby(lobby_id):
+    if lobby_id in lobbies.keys():
+        username = request.cookies.get('username')
+        if username:
+            lobbies[lobby_id].append(username)
+            return render_template('lobby.html', id=lobby_id, user=username, async_mode="threading")
+        else:
+            return redirect('/')
+    else:
+        return "Invalid Lobby Code"
+
+
+@app.get('/create-lobby')
+def create_lobby():
+    lobby_id = generate_lobby_id()
+    username = request.cookies.get('username')
+    if username:
+        lobbies[lobby_id] = [username]
+        return render_template('lobby.html', id=lobby_id, user=username, async_mode="threading")
+    else:
+        return redirect('/')
+
+
+def generate_lobby_id():
+    return ''.join(random.choice("abcdefghiklmopqrstuvwxyz1234567890") for _ in range(6))
+
+
+def escape_html(text):
+    """Returns a version of the input string with escaped html."""
+    return text.replace('&', '&amp').replace('<', '&lt').replace('>', '&gt')
+
+
+@socket.event()
+def join(data):
+    username = data['username']
+    room_id = data['room']
+    join_room(room_id)
+    print(room_id, flush=True)
+    emit('joined', f"{username} has joined!", to=room_id)
+
+
+if __name__ == '__main__':
+    socket.run(app, allow_unsafe_werkzeug=True, debug=True)
